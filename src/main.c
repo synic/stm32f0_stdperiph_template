a2fcb7d573f232684f9d252c262c34ccefaf27c8
Adam Olsen <arolsen@gmail.com>
2014-09-29 13:38:41 -0600

    started repo

---
+++ src/main.c
@@ -0,0 +1,82 @@
+/*
+ * A skeleton main.c
+ * Add your own code!
+ */
+
+// Load CMSIS and peripheral library and configuration
+#include "stm32f0xx.h"
+
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file , uint32_t line)
+{
+  /* Infinite loop */
+  /* Use GDB to find out why we're here */
+  while (1);
+}
+#endif
+
+
+/**
+  * @brief  Configure the GPIO Pins.
+  * @param  None
+  * @retval None
+  */
+void GPIO_Configuration(void)
+{
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+}
+
+
+/**
+  * @brief  Configure the nested vectored interrupt controller.
+  * @param  None
+  * @retval None
+  */
+void NVIC_Configuration(void)
+{
+}
+
+
+/**
+  * @brief  Configure the timers.
+  * @param  None
+  * @retval None
+  */
+void TIM_Configuration(void)
+{
+}
+
+void delay(long delay) {
+    delay *= 1000;
+    while(delay--) {}
+}
+
+int main(void)
+{
+  // Setup STM32 system (clock, PLL and Flash configuration)
+  SystemInit();
+
+  GPIO_Configuration();
+  //NVIC_Configuration();
+  //TIM_Configuration();
+
+  while (1)
+  {
+    GPIO_SetBits(GPIOC, GPIO_Pin_8);
+    delay(500);
+    GPIO_ResetBits(GPIOC, GPIO_Pin_8);
+    delay(500);
+  }
+
+  return 0;
+}
+
